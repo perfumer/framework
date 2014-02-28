2404c27e6a94e8c83163b9afbdd4ff9480d5f481
blumfontein <mehmatovec@gmail.com>
2014-02-28 18:10:12 +0000

    Getters and setters for Request class

--- lib/Request.php
+++ lib/Request.php
@@ -3,7 +3,7 @@
 namespace Perfumer;
 
 use Perfumer\Container\Core as Container;
-use Perfumer\Controller\HTTPException;
+use Perfumer\Controller\Exception\HTTPException;
 
 class Request
 {
@@ -31,26 +31,92 @@ class Request
         $url = trim($url, '/');
         $path = explode('/', $url);
 
-        $this->url = $url;
-        $this->method = ($method === null) ? strtolower($_SERVER['REQUEST_METHOD']) : $method;
-        $this->template = $url . '/' . $this->method . '.twig';
-        $this->css = $url . '/' . $this->method . '.css';
-        $this->js = $url . '/' . $this->method . '.js';
-        $this->controller = 'App\\Controller\\' . implode('\\', array_map('ucfirst', $path)) . 'Controller';
+        $this->setURL($url);
+        $this->setMethod(($method === null) ? strtolower($_SERVER['REQUEST_METHOD']) : $method);
+        $this->setTemplate($url . '/' . $this->method . '.twig');
+        $this->setCSS($url . '/' . $this->method . '.css');
+        $this->setJS($url . '/' . $this->method . '.js');
+        $this->setController('App\\Controller\\' . implode('\\', array_map('ucfirst', $path)) . 'Controller');
 
         try
         {
-            $reflection_class = new \ReflectionClass($this->controller);
+            $reflection_class = new \ReflectionClass($this->getController());
         }
         catch (\ReflectionException $e)
         {
-            throw new HTTPException("Controller '{$this->controller}' does not exist", 404);
+            throw new HTTPException("Controller '{$this->getController()}' does not exist", 404);
         }
 
         $request = $this;
         $response = $this->container->s('response');
         $controller = $reflection_class->newInstance($this->container, $request, $response);
 
-        return $reflection_class->getMethod('execute')->invoke($controller, $this->method, $args);
+        return $reflection_class->getMethod('execute')->invoke($controller, $this->getMethod(), $args);
+    }
+
+    public function getURL()
+    {
+        return $this->url;
+    }
+
+    public function setURL($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    public function setController($controller)
+    {
+        $this->controller = $controller;
+        return $this;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function setMethod($method)
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+
+    public function setTemplate($template)
+    {
+        $this->template = $template;
+        return $this;
+    }
+
+    public function getCSS()
+    {
+        return $this->css;
+    }
+
+    public function setCSS($css)
+    {
+        $this->css = $css;
+        return $this;
+    }
+
+    public function getJS()
+    {
+        return $this->js;
+    }
+
+    public function setJS($js)
+    {
+        $this->js = $js;
+        return $this;
     }
 }
