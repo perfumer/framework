6856198f84fe304033784fb62556267c28e90d39
blumfontein <mehmatovec@gmail.com>
2014-03-20 09:25:30 +0000

    Assets vendor getter-setter

--- lib/Assets.php
+++ lib/Assets.php
@@ -4,14 +4,18 @@ namespace Perfumer;
 
 class Assets
 {
+    protected $vendor_path;
     protected $css_path;
     protected $js_path;
 
+    protected $vendor_css = [];
+    protected $vendor_js = [];
     protected $css = [];
     protected $js = [];
 
     public function __construct(array $params)
     {
+        $this->vendor_path = '/' . trim($params['vendor_path'], '/');
         $this->css_path = '/' . trim($params['css_path'], '/');
         $this->js_path = '/' . trim($params['js_path'], '/');
     }
@@ -19,6 +23,9 @@ class Assets
     public function getCss()
     {
         $array = [];
+
+        foreach ($this->vendor_css as $css)
+            $array[] = $this->vendor_path . '/' . $css . '.css';
 
         foreach ($this->css as $css)
             $array[] = $this->css_path . '/' . $css . '.css';
@@ -29,6 +36,9 @@ class Assets
     public function getJs()
     {
         $array = [];
+
+        foreach ($this->vendor_js as $js)
+            $array[] = $this->vendor_path . '/' . $js . '.js';
 
         foreach ($this->js as $js)
             $array[] = $this->js_path . '/' . $js . '.js';
@@ -48,6 +58,22 @@ class Assets
     {
         if (!in_array($js, $this->js))
             $this->js[] = $js;
+
+        return $this;
+    }
+
+    public function addVendorCss($css)
+    {
+        if (!in_array($css, $this->vendor_css))
+            $this->vendor_css[] = $css;
+
+        return $this;
+    }
+
+    public function addVendorJs($js)
+    {
+        if (!in_array($js, $this->vendor_js))
+            $this->vendor_js[] = $js;
 
         return $this;
     }
