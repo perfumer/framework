29e83ff5ea1475a4d6bde6a6bcd13a118ba8c21e
blumfontein <mehmatovec@gmail.com>
2014-08-10 17:33:00 +0000

    Implement ldap port and suffix

--- lib/Auth/Driver/LdapDriver.php
+++ lib/Auth/Driver/LdapDriver.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class LdapDriver extends DatabaseDriver
 {
     protected $ldap_hostname;
+    protected $ldap_port = 636;
     protected $ldap_domain;
     protected $pull_user = false;
 
@@ -19,6 +20,9 @@ class LdapDriver extends DatabaseDriver
 
         if (isset($options['ldap_hostname']))
             $this->ldap_hostname = $options['ldap_hostname'];
+
+        if (isset($options['ldap_port']))
+            $this->ldap_port = $options['ldap_port'];
 
         if (isset($options['ldap_domain']))
             $this->ldap_domain = $options['ldap_domain'];
@@ -43,12 +47,12 @@ class LdapDriver extends DatabaseDriver
 
             if (!$force_login)
             {
-                $connection = ldap_connect($this->ldap_hostname);
+                $connection = ldap_connect($this->ldap_hostname, $this->ldap_port);
 
                 if (!$connection)
                     throw new AuthException(self::STATUS_REMOTE_SERVER_ERROR);
 
-                if (!@ldap_bind($connection, $this->ldap_domain . '\\' . $username, $password))
+                if (!@ldap_bind($connection, $username . '@' . $this->ldap_domain, $password))
                     throw new AuthException(self::STATUS_INVALID_CREDENTIALS);
             }
         }
