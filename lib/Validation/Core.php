3db79965a15e5208aaf5aaab6b460878c6886425
blumfontein <mehmatovec@gmail.com>
2014-09-11 18:30:13 +0600

    New Validation component based on Respect/Validation library

---
+++ lib/Validation/Core.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Perfumer\Validation;
+
+use Perfumer\I18n\Core as I18n;
+use Respect\Validation\Validator;
+
+class Core
+{
+    protected $i18n;
+
+    protected $rules = [];
+    protected $messages = [];
+
+    public function __construct(I18n $i18n)
+    {
+        $this->i18n = $i18n;
+    }
+
+    public function addRule($field, Validator $validator)
+    {
+        $this->rules[$field][] = $validator;
+
+        return $this;
+    }
+
+    public function validate($array)
+    {
+        $this->messages = [];
+
+        foreach ($array as $field => $value)
+        {
+            if (isset($this->rules[$field]))
+            {
+                foreach ($this->rules[$field] as $validator)
+                {
+                    try
+                    {
+                        if ($value === null)
+                            $value = '';
+
+                        $validator->assert($value);
+                    }
+                    catch (\InvalidArgumentException $e)
+                    {
+                        $messages = [];
+
+                        foreach ($validator->getRules() as $rule)
+                        {
+                            $name = $rule->getName();
+
+                            if (!$name)
+                                $name = lcfirst(preg_replace('/.*\\\/', '', get_class($rule)));
+
+                            $messages[$name] = $this->i18n->translate('_validation.' . $name);
+                        }
+
+                        foreach ($e->findMessages($messages) as $message)
+                        {
+                            if ($message)
+                                $this->messages[$field][] = $message;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count($this->messages) === 0;
+    }
+
+    public function getMessages()
+    {
+        $messages = [];
+
+        foreach ($this->messages as $field => &$message)
+        {
+            if (count($message) > 1)
+                $messages[$field] = $message;
+            else
+                $messages[$field] = $message[0];
+        }
+
+        return $messages;
+    }
+}
