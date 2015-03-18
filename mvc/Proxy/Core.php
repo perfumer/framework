a4daab99473950d3b5f1192833811c8c4294e77d
blumfontein <mehmatovec@gmail.com>
2015-03-18 11:30:16 +0600

    External router for http requests

--- mvc/Proxy/Core.php
+++ mvc/Proxy/Core.php
@@ -3,9 +3,7 @@
 namespace Perfumer\MVC\Proxy;
 
 use Perfumer\Component\Container\Core as Container;
-use Perfumer\Helper\Arr;
 use Perfumer\MVC\Proxy\Exception\ForwardException;
-use Perfumer\MVC\Proxy\Exception\ProxyException;
 
 class Core
 {
@@ -26,121 +24,20 @@ class Core
      */
     protected $next;
 
-    protected $input;
-
-    protected $http_prefixes = [];
-    protected $http_id;
-    protected $http_id_array;
-    protected $http_query = [];
-    protected $http_args = [];
+    protected $external_router;
 
     public function __construct(Container $container)
     {
         $this->container = $container;
-        $action = strtolower($_SERVER['REQUEST_METHOD']);
-
-        if ($prefixes = $container->getParam('proxy.prefixes'))
-        {
-            $this->http_prefixes = array_fill_keys($prefixes, null);
-
-            $prefix_options = $container->getParam('proxy.prefix_options');
-
-            if (is_array($prefix_options))
-            {
-                foreach ($prefixes as $prefix)
-                {
-                    if (isset($prefix_options[$prefix]['default_value']))
-                        $this->http_prefixes[$prefix] = $prefix_options[$prefix]['default_value'];
-                }
-            }
-        }
-
-        if ($_SERVER['PATH_INFO'] == '/')
-        {
-            $url = $container->getParam('proxy.default_url');
-        }
-        else
-        {
-            $url = trim($_SERVER['PATH_INFO'], '/');
-            $hyphen_pos = strpos($url, '-');
-
-            if ($hyphen_pos !== false)
-            {
-                $this->http_id = substr($url, $hyphen_pos + 1);
-                $url = substr($url, 0, $hyphen_pos);
-            }
-
-            if ($prefixes = $container->getParam('proxy.prefixes'))
-            {
-                $url = explode('/', $url);
-
-                $prefix_options = $container->getParam('proxy.prefix_options');
-
-                while ($prefixes && $url)
-                {
-                    $one_prefix = array_shift($prefixes);
-                    $one_url = $url[0];
-
-                    if ($this->validateUrlPartForPrefix($one_prefix, $one_url, $prefix_options))
-                    {
-                        $this->http_prefixes[$one_prefix] = $one_url;
-
-                        array_shift($url);
-                    }
-                }
-
-                $url = count($url) > 0 ? implode('/', $url) : $container->getParam('proxy.default_url');
-            }
-        }
-
-        $this->next = $container->getService('request')->init($url, $action);
-
-        $this->input = file_get_contents("php://input");
-
-        $data_type = $container->getParam('proxy.data_type');
-
-        // Get query parameters and args depending from type of data in the http request body
-        if ($data_type == 'query_string')
-        {
-            switch ($action)
-            {
-                case 'get':
-                    $this->http_query = $_GET;
-                    break;
-                case 'post':
-                    $this->http_query = $_GET;
-                    $this->http_args = $_POST;
-                    break;
-                default:
-                    $this->http_query = $_GET;
-                    parse_str($this->getInput(), $this->http_args);
-                    break;
-            }
-        }
-        else if ($data_type == 'json')
-        {
-            $this->http_query = $_GET;
-            $this->http_args = $this->getInput() ? json_decode($this->getInput(), true) : [];
-
-            if (!is_array($this->http_args))
-                $this->http_args = [];
-        }
-
-        // Trim all args if auto_trim setting enabled
-        if ($this->container->getParam('proxy.auto_trim'))
-        {
-            $this->http_args = Arr::trim($this->http_args);
-        }
-
-        // Convert empty strings to null values if auto_null setting enabled
-        if ($this->container->getParam('proxy.auto_null'))
-        {
-            $this->http_args = Arr::convertValues($this->http_args, '', null);
-        }
+        $this->external_router = $container->getService('external_router');
     }
 
     public function run()
     {
+        list($url, $action, $args) = $this->external_router->dispatch();
+
+        $this->next = $this->container->getService('request')->init($url, $action, $args);
+
         $this->start()->send();
     }
 
@@ -170,194 +67,9 @@ class Core
         return $this->request_pool[0];
     }
 
-    public function getInput()
-    {
-        return $this->input;
-    }
-
-    public function getPrefix($name = null, $default = null)
-    {
-        if ($name === null)
-            return $this->http_prefixes;
-
-        return isset($this->http_prefixes[$name]) ? $this->http_prefixes[$name] : $default;
-    }
-
-    public function setPrefix($name, $value)
-    {
-        if (!in_array($name, $this->container->getParam('proxy.prefixes')))
-            throw new ProxyException('Prefix "' . $name . '" is not registered in configuration');
-
-        $this->http_prefixes[$name] = $value;
-
-        return $this;
-    }
-
-    public function getId($index = null)
-    {
-        if ($index === null)
-            return $this->http_id;
-
-        if ($this->http_id_array === null)
-            $this->http_id_array = explode('/', $this->http_id);
-
-        return isset($this->http_id_array[$index]) ? $this->http_id_array[$index] : null;
-    }
-
-    public function setId($id, $index = null)
-    {
-        if ($index === null)
-        {
-            $this->http_id = $id;
-        }
-        else
-        {
-            if ($this->http_id_array === null)
-                $this->http_id_array = explode('/', $this->http_id);
-
-            $this->http_id_array[$index] = $id;
-        }
-
-        return $this;
-    }
-
-    public function getArg($name = null, $default = null)
-    {
-        if ($name === null)
-            return $this->http_args;
-
-        return isset($this->http_args[$name]) ? $this->http_args[$name] : $default;
-    }
-
-    public function hasArgs()
-    {
-        return count($this->http_args) > 0;
-    }
-
-    public function setArg($name, $value)
-    {
-        $this->http_args[$name] = $value;
-
-        return $this;
-    }
-
-    public function setArgsArray($array)
-    {
-        $this->http_args = $array;
-
-        return $this;
-    }
-
-    public function addArgsArray($array)
-    {
-        $this->http_args = array_merge($this->http_args, $array);
-
-        return $this;
-    }
-
-    public function deleteArgs(array $keys = [])
-    {
-        if ($keys)
-        {
-            foreach ($keys as $key)
-            {
-                if (isset($this->http_args[$key]))
-                    unset($this->http_args[$key]);
-            }
-        }
-        else
-        {
-            $this->http_args = [];
-        }
-
-        return $this;
-    }
-
-    public function getQuery($name = null, $default = null)
-    {
-        if ($name === null)
-            return $this->http_query;
-
-        return isset($this->http_query[$name]) ? $this->http_query[$name] : $default;
-    }
-
-    public function hasQuery()
-    {
-        return count($this->http_query) > 0;
-    }
-
-    public function setQuery($name, $value)
-    {
-        $this->http_query[$name] = $value;
-
-        return $this;
-    }
-
-    public function setQueryArray($array)
-    {
-        $this->http_query = $array;
-
-        return $this;
-    }
-
-    public function addQueryArray($array)
-    {
-        $this->http_query = array_merge($this->http_query, $array);
-
-        return $this;
-    }
-
-    public function deleteQuery(array $keys = [])
-    {
-        if ($keys)
-        {
-            foreach ($keys as $key)
-            {
-                if (isset($this->http_query[$key]))
-                    unset($this->http_query[$key]);
-            }
-        }
-        else
-        {
-            $this->http_query = [];
-        }
-
-        return $this;
-    }
-
     public function generateUrl($url, $id = null, $query = [], $prefixes = [])
     {
-        $generated_url = '/' . trim($url, '/');
-
-        if ($this->container->getParam('proxy.prefixes'))
-        {
-            if ($prefixes)
-            {
-                $prefixes = Arr::fetch($prefixes, $this->container->getParam('proxy.prefixes'));
-                $prefixes = array_merge($this->getPrefix(), $prefixes);
-            }
-            else
-            {
-                $prefixes = $this->getPrefix();
-            }
-
-            $generated_url = '/' . implode('/', $prefixes) . $generated_url;
-        }
-
-        if ($id)
-            $generated_url .= is_array($id) ? '-' . implode('/', $id) : '-' . $id;
-
-        if ($query)
-        {
-            $query_parts = [];
-
-            foreach ($query as $key => $value)
-                $query_parts[] = $key . '=' . urlencode($value);
-
-            $generated_url .= '?' . implode('&', $query_parts);
-        }
-
-        return $generated_url;
+        return $this->external_router->generateUrl($url, $id, $query, $prefixes);
     }
 
     /**
@@ -406,22 +118,103 @@ class Core
         return $reflection_class->getMethod('execute')->invoke($controller);
     }
 
-    protected function validateUrlPartForPrefix($prefix, $part, $options)
+    public function getPrefix($name = null, $default = null)
     {
-        if ($options === null || !isset($options[$prefix]))
-            return true;
+        return $this->external_router->getPrefix($name, $default);
+    }
 
-        $options = $options[$prefix];
+    public function setPrefix($name, $value)
+    {
+        $this->external_router->setPrefix($name, $value);
 
-        if (isset($options['white_list']))
-            return in_array($part, $options['white_list']);
+        return $this;
+    }
 
-        if (isset($options['black_list']))
-            return !in_array($part, $options['black_list']);
+    public function getId($index = null)
+    {
+        return $this->external_router->getId($index);
+    }
 
-        if (isset($options['regex']))
-            return preg_match($options['regex'], $part);
+    public function setId($id, $index = null)
+    {
+        $this->external_router->setId($id, $index);
 
-        return true;
+        return $this;
+    }
+
+    public function getArg($name = null, $default = null)
+    {
+        return $this->external_router->getArg($name, $default);
+    }
+
+    public function hasArgs()
+    {
+        return $this->external_router->hasArgs();
+    }
+
+    public function setArg($name, $value)
+    {
+        $this->external_router->setArg($name, $value);
+
+        return $this;
+    }
+
+    public function setArgsArray($array)
+    {
+        $this->external_router->setArgsArray($array);
+
+        return $this;
+    }
+
+    public function addArgsArray($array)
+    {
+        $this->external_router->addArgsArray($array);
+
+        return $this;
+    }
+
+    public function deleteArgs(array $keys = [])
+    {
+        $this->external_router->deleteArgs($keys);
+
+        return $this;
+    }
+
+    public function getQuery($name = null, $default = null)
+    {
+        return $this->external_router->getQuery($name, $default);
+    }
+
+    public function hasQuery()
+    {
+        return $this->external_router->hasQuery();
+    }
+
+    public function setQuery($name, $value)
+    {
+        $this->external_router->setQuery($name, $value);
+
+        return $this;
+    }
+
+    public function setQueryArray($array)
+    {
+        $this->external_router->setQueryArray($array);
+
+        return $this;
+    }
+
+    public function addQueryArray($array)
+    {
+        $this->external_router->addQueryArray($array);
+
+        return $this;
+    }
+
+    public function deleteQuery(array $keys = [])
+    {
+        $this->external_router->deleteQuery($keys);
+
+        return $this;
     }
 }
