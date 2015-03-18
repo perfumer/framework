bae22560eac69accd82993663c3b6fe44c4b8e5b
blumfontein <mehmatovec@gmail.com>
2015-03-18 16:26:37 +0600

    Directory internal router

---
+++ mvc/InternalRouter/DirectoryRouter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Perfumer\MVC\InternalRouter;
+
+use Perfumer\MVC\Proxy\Request;
+
+class DirectoryRouter
+{
+    protected $options = [];
+
+    public function __construct($options = [])
+    {
+        $default_options = [
+            'prefix' => 'App\\Controller',
+            'suffix' => 'Controller'
+        ];
+
+        $this->options = array_merge($default_options, $options);
+    }
+
+    public function dispatch($url, $action, $args = [])
+    {
+        $path = explode('/', $url);
+
+        $controller = $this->options['prefix'] . '\\' . implode('\\', array_map('ucfirst', $path)) . $this->options['suffix'];
+
+        $request = new Request();
+
+        $request->setUrl($url)->setController($controller)->setAction($action)->setArgs($args);
+
+        return $request;
+    }
+}
