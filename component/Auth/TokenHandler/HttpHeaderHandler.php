8f0f715a3c2e2168c03e6dae77d6b15faa248370
blumfontein <mehmatovec@gmail.com>
2015-04-24 14:17:56 +0600

    HTTP header token handler for auth service

---
+++ component/Auth/TokenHandler/HttpHeaderHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Perfumer\Component\Auth\TokenHandler;
+
+class HttpHeaderHandler extends AbstractHandler
+{
+    protected $header;
+
+    public function __construct($header)
+    {
+        $this->header = $header;
+    }
+
+    public function getToken()
+    {
+        return isset($_SERVER[$this->header]) ? $_SERVER[$this->header] : null;
+    }
+
+    public function setToken($token)
+    {
+    }
+
+    public function deleteToken()
+    {
+    }
+
+    public function getTokenLifetime()
+    {
+        return 30 * 86400;
+    }
+}
