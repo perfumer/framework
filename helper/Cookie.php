1ad2ebcb1400fdf3a0ae6cf5c20c1c411b492b96
blumfontein <mehmatovec@gmail.com>
2015-05-26 16:19:18 +0600

    Cookie helper refactoring

--- helper/Cookie.php
+++ helper/Cookie.php
@@ -4,48 +4,39 @@ namespace Perfumer\Helper;
 
 class Cookie
 {
-    protected $expiration = 3600;
-    protected $path = '/';
-    protected $domain = null;
-    protected $secure = false;
-    protected $httponly = true;
+    protected $options = [];
 
-    public function __construct(array $params = [])
+    public function __construct(array $options = [])
     {
-        if (isset($params['expiration']))
-            $this->expiration = (int) $params['expiration'];
+        $default_options = [
+            'expiration' => 3600,
+            'path' => '/',
+            'domain' => null,
+            'secure' => false,
+            'http_only' => true
+        ];
 
-        if (isset($params['path']))
-            $this->path = (string) $params['path'];
-
-        if (isset($params['domain']) && $params['domain'] !== null)
-            $this->domain = (string) $params['domain'];
-
-        if (isset($params['secure']))
-            $this->secure = (bool) $params['secure'];
-
-        if (isset($params['httponly']))
-            $this->httponly = (bool) $params['httponly'];
+        $this->options = array_merge($default_options, $options);
     }
 
     public function getPath()
     {
-        return $this->path;
+        return $this->options['path'];
     }
 
     public function getDomain()
     {
-        return $this->domain;
+        return $this->options['domain'];
     }
 
     public function getSecure()
     {
-        return $this->secure;
+        return $this->options['secure'];
     }
 
-    public function getHttponly()
+    public function getHttpOnly()
     {
-        return $this->httponly;
+        return $this->options['http_only'];
     }
 
     public function get($key, $default = null)
@@ -56,18 +47,18 @@ class Cookie
     public function set($name, $value, $expiration = null)
     {
         if ($expiration === null)
-            $expiration = $this->expiration;
+            $expiration = $this->options['expiration'];
 
         if ($expiration > 0)
             $expiration += time();
 
-        return setcookie($name, $value, $expiration, $this->path, $this->domain, $this->secure, $this->httponly);
+        return setcookie($name, $value, $expiration, $this->options['path'], $this->options['domain'], $this->options['secure'], $this->options['http_only']);
     }
 
     public function delete($name)
     {
         unset($_COOKIE[$name]);
 
-        return setcookie($name, null, -86400, $this->path, $this->domain, $this->secure, $this->httponly);
+        return setcookie($name, null, -86400, $this->options['path'], $this->options['domain'], $this->options['secure'], $this->options['http_only']);
     }
 }
