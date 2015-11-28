7c41a50370f77e6cfc19cc2be3a12d8e431763a8
blumfontein <mehmatovec@gmail.com>
2015-11-28 14:38:16 +0600

    Statuses in internal responses

--- framework/Proxy/Response.php
+++ framework/Proxy/Response.php
@@ -4,6 +4,8 @@ namespace Perfumer\Framework\Proxy;
 
 class Response
 {
+    protected $status = true;
+
     protected $content;
 
     public function getContent()
@@ -14,6 +16,18 @@ class Response
     public function setContent($content)
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = (bool) $status;
 
         return $this;
     }
