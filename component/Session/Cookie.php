e16eec529432c2775e3c9a5c8882ce5bf81043b2
blumfontein <mehmatovec@gmail.com>
2016-02-06 16:42:59 +0600

    Docblock and PSR-2 formatting for Cookie

--- component/Session/Cookie.php
+++ component/Session/Cookie.php
@@ -4,12 +4,19 @@ namespace Perfumer\Component\Session;
 
 class Cookie
 {
+    /**
+     * @var array
+     */
     protected $options = [];
 
+    /**
+     * Cookie constructor.
+     * @param array $options
+     */
     public function __construct(array $options = [])
     {
         $default_options = [
-            'expiration' => 3600,
+            'lifetime' => 3600,
             'path' => '/',
             'domain' => null,
             'secure' => false,
@@ -19,42 +26,71 @@ class Cookie
         $this->options = array_merge($default_options, $options);
     }
 
+    /**
+     * @return string
+     */
     public function getPath()
     {
         return $this->options['path'];
     }
 
+    /**
+     * @return string
+     */
     public function getDomain()
     {
         return $this->options['domain'];
     }
 
+    /**
+     * @return bool
+     */
     public function getSecure()
     {
         return $this->options['secure'];
     }
 
+    /**
+     * @return bool
+     */
     public function getHttpOnly()
     {
         return $this->options['http_only'];
     }
 
+    /**
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
     public function get($key, $default = null)
     {
         return isset($_COOKIE[$key]) ? $_COOKIE[$key] : $default;
     }
 
-    public function set($name, $value, $expiration = null)
+    /**
+     * @param string $name
+     * @param string $value
+     * @param int|null $lifetime
+     * @return bool
+     */
+    public function set($name, $value, $lifetime = null)
     {
-        if ($expiration === null)
-            $expiration = $this->options['expiration'];
+        if ($lifetime === null) {
+            $lifetime = $this->options['lifetime'];
+        }
 
-        if ($expiration > 0)
-            $expiration += time();
+        if ($lifetime > 0) {
+            $lifetime += time();
+        }
 
-        return setcookie($name, $value, $expiration, $this->options['path'], $this->options['domain'], $this->options['secure'], $this->options['http_only']);
+        return setcookie($name, $value, $lifetime, $this->options['path'], $this->options['domain'], $this->options['secure'], $this->options['http_only']);
     }
 
+    /**
+     * @param string $name
+     * @return bool
+     */
     public function delete($name)
     {
         unset($_COOKIE[$name]);
