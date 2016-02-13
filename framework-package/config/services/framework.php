9979d010f761dec035f925094b64587a975aaed7
blumfontein <mehmatovec@gmail.com>
2016-02-13 15:58:40 +0600

    Requesting, view, session, cache default definitions

--- framework-package/config/services/framework.php
+++ framework-package/config/services/framework.php
@@ -8,10 +8,125 @@ return [
     ],
 
     // Requesting
+    'bundle.http_router' => [
+        'shared' => true,
+        'class' => 'Perfumer\\Framework\\BundleRouter\\HttpRouter'
+    ],
+
+    'bundle.console_router' => [
+        'shared' => true,
+        'class' => 'Perfumer\\Framework\\BundleRouter\\ConsoleRouter'
+    ],
+
     'proxy' => [
         'shared' => true,
         'class' => 'Perfumer\\Framework\\Proxy\\Proxy',
         'arguments' => ['container']
+    ],
+
+    // View
+    'view.template' => [
+        'class' => 'Perfumer\\Framework\\View\\TemplateView',
+        'arguments' => ['#twig', '#bundler']
+    ],
+
+    'view.serialize' => [
+        'class' => 'Perfumer\\Framework\\View\\SerializeView',
+        'arguments' => ['json']
+    ],
+
+    // Twig
+    'twig' => [
+        'shared' => true,
+        'class' => 'Twig_Environment',
+        'arguments' => ['#twig.filesystem_loader']
+    ],
+
+    'twig.filesystem_loader' => [
+        'shared' => true,
+        'class' => 'Twig_Loader_Filesystem'
+    ],
+
+    'twig.framework_extension' => [
+        'class' => 'Perfumer\\Framework\\View\\TwigExtension\\FrameworkExtension',
+        'arguments' => ['container']
+    ],
+
+    'twig.http_router_extension' => [
+        'class' => 'Perfumer\\Framework\\View\\TwigExtension\\HttpRouterExtension',
+        'arguments' => ['container']
+    ],
+
+    'twig.console_router_extension' => [
+        'class' => 'Perfumer\\Framework\\View\\TwigExtension\\ConsoleRouterExtension',
+        'arguments' => ['container']
+    ],
+
+    // Session
+    'session' => [
+        'shared' => true,
+        'class' => 'Perfumer\\Component\\Session\\Pool',
+        'arguments' => ['#cache.memcache']
+    ],
+
+    'cookie' => [
+        'shared' => true,
+        'class' => 'Perfumer\\Component\\Session\\Cookie'
+    ],
+
+    // Cache
+    'cache.file' => [
+        'shared' => true,
+        'class' => 'Stash\\Pool',
+        'arguments' => ['#cache.file_driver']
+    ],
+
+    'cache.file_driver' => [
+        'shared' => true,
+        'class' => 'Stash\\Driver\\FileSystem',
+        'after' => function(\Perfumer\Component\Container\Container $container, \Stash\Driver\FileSystem $driver) {
+            $driver->setOptions();
+        }
+    ],
+
+    'cache.memcache' => [
+        'shared' => true,
+        'class' => 'Stash\\Pool',
+        'arguments' => ['#cache.memcache_driver']
+    ],
+
+    'cache.memcache_driver' => [
+        'shared' => true,
+        'class' => 'Stash\\Driver\\Memcache',
+        'after' => function(\Perfumer\Component\Container\Container $container, \Stash\Driver\Memcache $driver) {
+            $driver->setOptions();
+        }
+    ],
+
+    'cache.ephemeral' => [
+        'shared' => true,
+        'class' => 'Stash\\Pool',
+        'arguments' => ['#cache.ephemeral_driver']
+    ],
+
+    'cache.ephemeral_driver' => [
+        'shared' => true,
+        'class' => 'Stash\\Driver\\Ephemeral'
+    ],
+
+    // Translator
+    'translator' => [
+        'shared' => true,
+        'class' => 'Perfumer\\Component\\Translator\\Core',
+        'arguments' => ['#cache', [
+            'locale' => 'ru'
+        ]]
+    ],
+
+    // Validation
+    'validation' => [
+        'class' => 'Perfumer\\Component\\Validation\\Core',
+        'arguments' => ['#translator']
     ],
 
     // Framework bundle default routers
