f88305377e78d080c960b65bd280b3e876235ab7
blumfontein <mehmatovec@gmail.com>
2016-02-13 15:42:33 +0600

    Console bundle router settings

--- framework-package/controller/Exception/PlainController.php
+++ framework-package/controller/Exception/PlainController.php
@@ -29,4 +29,28 @@ class PlainController extends BaseController
 
         $this->getResponse()->setContent('Action not found');
     }
+
+    public function isLogged()
+    {
+        if ($this->getExternalRouter()->isHttp())
+            $this->getExternalResponse()->setStatusCode(403);
+
+        $this->getResponse()->setContent('Access to this page is permitted to logged in users only.');
+    }
+
+    public function isAdmin()
+    {
+        if ($this->getExternalRouter()->isHttp())
+            $this->getExternalResponse()->setStatusCode(403);
+
+        $this->getResponse()->setContent('Access to this page is permitted to administrators only.');
+    }
+
+    public function isGranted()
+    {
+        if ($this->getExternalRouter()->isHttp())
+            $this->getExternalResponse()->setStatusCode(403);
+
+        $this->getResponse()->setContent('You do not have enough rights to access this page.');
+    }
 }
--- framework/BundleRouter/ConsoleRouter.php
+++ framework/BundleRouter/ConsoleRouter.php
@@ -4,8 +4,26 @@ namespace Perfumer\Framework\BundleRouter;
 
 class ConsoleRouter implements RouterInterface
 {
+    protected $bundles = [];
+
+    public function __construct($bundles = [])
+    {
+        $this->bundles = $bundles;
+    }
+
     public function dispatch()
     {
-        return $_SERVER['argv'][1];
+        $bundle = $_SERVER['argv'][1];
+
+        foreach ($this->bundles as $route)
+        {
+            if ($route['domain'] === $_SERVER['argv'][1])
+            {
+                $bundle = $route['bundle'];
+                break;
+            }
+        }
+
+        return $bundle;
     }
 }
