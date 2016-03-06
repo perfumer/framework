2fc81726fde550301f573aca71c783fde309e5c8
blumfontein <mehmatovec@gmail.com>
2016-03-06 19:36:55 +0600

    Exception controllers microoptimizations

--- framework-package/controller/Exception/PlainController.php
+++ framework-package/controller/Exception/PlainController.php
@@ -2,55 +2,69 @@
 
 namespace Perfumer\FrameworkPackage\Controller\Exception;
 
-use Perfumer\Framework\Controller\PlainController as BaseController;
+use Perfumer\Framework\Controller\AbstractController;
 
-class PlainController extends BaseController
+class PlainController extends AbstractController
 {
     public function pageNotFound()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(404);
+        }
 
         $this->getResponse()->setContent('Page not found');
     }
 
     public function controllerNotFound()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(404);
+        }
 
         $this->getResponse()->setContent('Controller not found');
     }
 
     public function actionNotFound()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(404);
+        }
 
         $this->getResponse()->setContent('Action not found');
     }
 
     public function isLogged()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(403);
+        }
 
         $this->getResponse()->setContent('Access to this page is permitted to logged in users only.');
     }
 
     public function isAdmin()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(403);
+        }
 
         $this->getResponse()->setContent('Access to this page is permitted to administrators only.');
     }
 
     public function isGranted()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(403);
+        }
 
         $this->getResponse()->setContent('You do not have enough rights to access this page.');
+    }
+
+    protected function pageNotFoundException()
+    {
+    }
+
+    protected function actionNotFoundException()
+    {
     }
 }
--- framework-package/controller/Exception/SerializeController.php
+++ framework-package/controller/Exception/SerializeController.php
@@ -9,48 +9,54 @@ class SerializeController extends BaseController
 {
     public function pageNotFound()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(404);
+        }
 
         $this->setErrorMessage('Page not found.');
     }
 
     public function controllerNotFound()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(404);
+        }
 
         $this->setErrorMessage('Controller not found.');
     }
 
     public function actionNotFound()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(404);
+        }
 
         $this->setErrorMessage('Action not found.');
     }
 
     public function isLogged()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(403);
+        }
 
         $this->setErrorMessage('Access to this page is permitted to logged in users only.');
     }
 
     public function isAdmin()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(403);
+        }
 
         $this->setErrorMessage('Access to this page is permitted to administrators only.');
     }
 
     public function isGranted()
     {
-        if ($this->getExternalRouter()->isHttp())
+        if ($this->getExternalRouter()->isHttp()) {
             $this->getExternalResponse()->setStatusCode(403);
+        }
 
         $this->setErrorMessage('You do not have enough rights to access this page.');
     }
