6f4688699953b00ca80a4b30f0909f67aec7be2a
blumfontein <mehmatovec@gmail.com>
2016-06-03 16:15:51 +0600

    Console exception command

---
+++ package/command/ExceptionCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Perfumer\Package\Command;
+
+use Perfumer\Framework\Controller\PlainController;
+
+class ExceptionCommand extends PlainController
+{
+    public function pageNotFound()
+    {
+        $this->getResponse()->setContent('Page not found');
+    }
+
+    public function isLogged()
+    {
+        $this->getResponse()->setContent('Access to this page is permitted to logged in users only.');
+    }
+
+    public function isAdmin()
+    {
+        $this->getResponse()->setContent('Access to this page is permitted to administrators only.');
+    }
+
+    public function isGranted()
+    {
+        $this->getResponse()->setContent('You do not have enough rights to access this page.');
+    }
+}
